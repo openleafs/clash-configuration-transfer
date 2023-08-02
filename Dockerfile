d0e1433c4fc150c028fc4cca1431c1f95d057a0e
windows@home <programlfyh@proton.me>
2023-08-02 23:13:09 +0800

    - Added   - add Dockerfile - Changed   - change transfer function in app.py - Fixed

---
+++ Dockerfile
@@ -0,0 +1,19 @@
+# Use a base Python image
+FROM ubuntu:latest
+
+RUN apt-get update && apt-get install -y python3 python3-pip
+
+# Set the working directory in the container
+WORKDIR /app
+
+# Copy the Python application files into the container
+COPY . .
+
+# Install application dependencies
+RUN pip install --no-cache-dir -r requirements.txt
+
+# Expose the port your Flask application is running on
+EXPOSE 5000
+
+# Define the command to run your Python server
+CMD ["python3", "app.py"]
--- app.py
+++ app.py
@@ -9,15 +9,22 @@ app = Flask(__name__)
 # Route decorator to handle GET requests at the root URL "/"
 @app.route("/transfer", methods=["GET"])
 def transfer():
-    sub_link = request.args.get("sub_link")
-    custom_link = request.args.get("custom_link")
-    sub_link_transfer = SubLinkTransfer(sub_link, custom_link)
-    self_clash_configuration = sub_link_transfer.get_result()
+    try:
+        sub_link = request.args.get("sub_link")
+        custom_link = request.args.get("custom_link")
 
-    yaml_data = yaml.dump(self_clash_configuration)
-    response = Response(yaml_data, content_type="text/plain;charset=utf-8")
+        if sub_link is None or custom_link is None:
+            return "please offer both sub link and custom link"
 
-    return response
+        sub_link_transfer = SubLinkTransfer(sub_link, custom_link)
+        self_clash_configuration = sub_link_transfer.get_result()
+
+        yaml_data = yaml.dump(self_clash_configuration)
+        response = Response(yaml_data, content_type="text/plain; charset=utf-8")
+
+        return response
+    except Exception as e:
+        return str(e)
 
 
 if __name__ == '__main__':
