34b0fb79aa8c371d56e07eec8bc4a68252a01c53
windows@home <programlfyh@proton.me>
2023-08-04 21:38:23 +0800

    Feat: add easy bash script for docker build image and run container

---
+++ rebuild_clash_docker.sh
@@ -0,0 +1,15 @@
+# !/bin/bash
+sudo docker stop clash
+echo "stop container clash"
+
+sudo docker rm clash
+echo "rm container clash"
+
+sudo docker image rm clash/config-transfer
+echo "rm image clash/config-transfer"
+
+sudo docker build -t clash/config-transfer .
+echo "build a new image: clash/config-transfer"
+
+sudo docker run -d -p 5000:5000 --name clash clash/config-transfer
+echo "run a new contianer: clash"
